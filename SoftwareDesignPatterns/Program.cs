7f3b8a95dc3995bc533a5e0bf0cf2d225bcf4a81
CristianSifuentesNeo <cristian.sifuentes@neoris.com>
2023-10-25 16:50:39 -0600

    Creating example for singleton pattern

--- SoftwareDesignPatterns/Program.cs
+++ SoftwareDesignPatterns/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Intenta obtener la instancia del Singleton.
+        Singleton singleton1 = Singleton.GetInstance();
+        Singleton singleton2 = Singleton.GetInstance();
+
+        // Comprueba si las dos instancias son la misma.
+        if (singleton1 == singleton2)
+        {
+            Console.WriteLine("Ambas instancias son idénticas. Esto demuestra que es un Singleton.");
+        }
+        
+        // Ejecuta la lógica de negocio en la instancia del Singleton.
+        singleton1.SomeBusinessLogic();
+    }
+}
---
+++ SoftwareDesignPatterns/Singleton.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Singleton
+{
+    private static Singleton instance;
+    
+    // Un constructor privado para evitar la creación de instancias desde fuera de la clase.
+    private Singleton() { }
+    
+    // Un método público que devuelve la única instancia de la clase.
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void SomeBusinessLogic()
+    {
+        Console.WriteLine("Ejecutando la lógica de negocio en Singleton.");
+    }
+}
