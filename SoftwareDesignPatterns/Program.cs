e9e6b9598f6a4ae6598dfaedba7d907a9d7104dc
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-10-31 13:35:17 -0600

    Creating example using lazy list

--- SoftwareDesignPatterns/Program.cs
+++ SoftwareDesignPatterns/Program.cs
@@ -30,6 +30,24 @@ class Program
             });
             thread.Start();
         }
+
+        
+        Lazy<List<int>> lazyList = new Lazy<List<int>>(() => 
+        {
+            var list = new List<int>();
+            // Realizar alguna operación costosa para llenar la lista.
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        });
+
+       
+       // En algún otro lugar del código, cuando se necesita la lista:
+       List<int> lista = lazyList.Value; // La lista se inicializa en este momento, si aún no se ha hecho.
+
+
     }
 }
 
